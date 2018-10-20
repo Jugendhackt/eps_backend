2992a28dfe46d50b6b1e6b051eec7a36e0cd69d0
PeterHeynmoeller <peddyheynmoeller@gmail.com>
2018-10-20 17:08:21 +0200

    yet another bit of security

--- logic/signup.js
+++ logic/signup.js
@@ -12,20 +12,28 @@ module.exports = {
       if(error){
         console.log("Unable to connect to database, error: ", error);
       }else {
-        db.collection('user').update({
-          username: uname,
-          password: pass
-       }, {
-          username: uname,
-         password: pass
-       },{
-         upsert: true
-       }).then(function(result){
-         console.log("Updated/Inserted")
-         callback(true);
-         db.close();
-       });
-
+        db.collection('user').find({username: uname}).toArray(function(error, result){
+          if(error){
+            callback(false);
+          }else if(result.length){
+            console.log(result);
+            callback(false);
+          }else{
+             db.collection('user').update({
+                username: uname,
+                password: pass
+             }, {
+                username: uname,
+               password: pass
+             },{
+               upsert: true
+             }).then(function(result){
+               console.log("Updated/Inserted")
+               callback(true);
+               db.close();
+             });
+          }
+        });
       }
     });
   }
